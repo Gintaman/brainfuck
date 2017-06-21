09af30c972fb40fffef9c5455a6a8171d7e0df43
jeffreyc <jeffrey_chen@whirlpool.com>
2017-06-21 18:43:26 -0400

    ported interpreter to c

--- brainfuck.c
+++ brainfuck.c
@@ -3,35 +3,109 @@
 #include "brainfuck.h"
 
 int main(int argc, char **argv) {
-    uint8_t *ptr = NULL;
-    ptr = (uint8_t *) calloc(30000, sizeof(char));
+    //tape pointer
+    uint8_t *tp = NULL;
+    tp = (uint8_t *) calloc(30000, sizeof(char));
 
     FILE *file = NULL;
     char source[1024];
-    size_t read;
+
     file = fopen("source.b", "r");
     if(file) {
-        while((read = fread(source, 1, sizeof(source), file)) > 0) {
+        char c;
+        int curr = 0;
+        while((c = fgetc(file)) != EOF) {
+            source[curr] = c;
+            curr++;
         }
         if(ferror(file)) {
             fprintf(stderr, "file error\n");
+            free(tp);
             exit(1);
         }
         fclose(file);
     }
 
-    if(ptr == NULL) {
+    if(tp == NULL) {
         fprintf(stderr, "calloc failed\n");
+        free(tp);
         exit(1);
     }
 
-	char *tp = &source[0];
+    //source pointer
+	char *sp = &source[0];
 
-	run(ptr, source);
+	run(tp, sp);
 
     return 0;
 }
 
-void run(uint8_t* ptr, char* source) {
-    printf("test: %u %c\n", ptr[2], source[2]);
+//pointer to tape (memory cells), pointer to source code
+void run(uint8_t* tp, char* sp) {
+    int sourcePointer = 0;
+    int tapePointer = 0;
+    char tok;
+    int count = 0;
+    while(sp[sourcePointer] != 0) {
+        count++;
+        tok = sp[sourcePointer];
+        switch(tok) {
+            case '>':
+                if(tapePointer < 30000) {
+                    tapePointer++;
+                }
+                break;
+            case '<':
+                if(tapePointer > 0) {
+                    tapePointer--;
+                }
+                break;
+            case '+':
+                tp[tapePointer] = tp[tapePointer] + 1;
+                break;
+            case '-':
+                tp[tapePointer] = tp[tapePointer] - 1;
+                break;
+            case '.':
+                printf("%c", tp[tapePointer]);
+                break;
+            case ',':
+                break;
+            case '[':
+                if(tp[tapePointer] == 0) {
+                    int bal = 1;
+                    while(bal > 0) {
+                        sourcePointer++;
+                        if(sp[sourcePointer] == '[') {
+                            bal++;
+                        }
+                        else if(sp[sourcePointer] == ']') {
+                            bal--;
+                        }
+                    }
+                    sourcePointer++;
+                }
+                break;
+            case ']':
+                if(tp[tapePointer] != 0) {
+                    int bal = 1;
+                    while(bal > 0) {
+                        sourcePointer--;
+                        if(sp[sourcePointer] == '[') {
+                            bal--;
+                        }
+                        else if(sp[sourcePointer] == ']') {
+                            bal++;
+                        }
+                    }
+                }
+                break;
+        }
+        sourcePointer++;
+        if(count > 5000) {
+            fprintf(stderr, "looping too much\n");
+            exit(1);
+        }
+    }
+    printf("Program execution completed.\n");
 }
